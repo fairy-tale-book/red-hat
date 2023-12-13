1147d13f798e03c1608c2c18b5db48f9e6853212
xsf0105 <allan91coder@gmail.com>
2023-12-13 11:15:04 +0800

    init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,31 @@
-// https://typescript-eslint.io/getting-started
-
-
-/* eslint-env node */
 module.exports = {
-  root: true,
-  extends: ['eslint:recommended'],
+  // root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    "semi": ["error", "always"],
+    "quotes": ["error", "double"],
+    'no-plusplus': 0,
+    'max-len': 0,
+    "no-param-reassign": "off",
+  },
 };
